e30c061feff7e0cfa1ba8d37da461b0132b4bcbc
Larry <80938629+Larry-vt@users.noreply.github.com>
2023-04-23 00:03:30 -0500

    Products_Tabs Added tabs to product page Still working on moblie version and need to add pictures and product descriptions
    also something note a small error occurs if you click the already selected tab- it will hide it's contents- I am assumings its from my java scrip code that I'm trying to use for the mobile version but I'm not sure.

--- Client/mobile.js
+++ Client/mobile.js
@@ -10,3 +10,38 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
+
+
+
+  /* used for on mobile. 
+     It will display only the tabs and whatever is clicked on it will display only its contents
+     Tabs will disappear. but there will be a back button of some sort for the user to click on to view other tabs */
+$(document).ready(function() {
+  $('.nav-tabs .nav-link').on('click', function() {
+    //if ($(window).width() < 768) {
+      var target = $(this).attr('href');
+      $(target).toggle();
+      $(target).siblings().hide();
+    //}
+  });
+});
+
+
+// $(document).ready(function() {
+//   // Show and hide tab content on mobile devices
+//   $('#tab-menu a').on('click', function(e) {
+//     e.preventDefault();
+//     var tabId = $(this).attr('href');
+//     $(this).tab('show');
+//     $(tabId).show();
+//     $('.tab-content').not(tabId).hide();
+//     $('#tab-menu').hide();
+//     $('#back-button').show();
+//   });
+
+//   $('#back-button').on('click', function() {
+//     $('.tab-content').hide();
+//     $('#tab-menu').show();
+//     $('#back-button').hide();
+//   });
+// });
